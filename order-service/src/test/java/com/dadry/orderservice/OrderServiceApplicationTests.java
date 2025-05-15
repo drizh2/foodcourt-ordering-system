6d89d6c861498d9e601993a8d87f03bb3138e8ad
drizh2 <drizh2@ukr.net>
2025-05-15 13:58:02 +0300

    Initial commit

---
+++ order-service/src/test/java/com/dadry/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadry.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
